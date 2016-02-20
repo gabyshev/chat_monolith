42421fa15d20283011a43aa7edc85353de12d872
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-02-20 21:43:46 +0600

    create conversation or return existing one

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,5 +1,18 @@
 class ConversationsController < ApplicationController
+  respond_to :json
+
   def index
-    @conversation = current_user.conversations
+    @users = User.where.not(id: current_user.id)
+  end
+
+  def create
+    sender    = User.find(params[:sender_id])
+    recipient = User.find(params[:recipient_id])
+    @conversation = unless Conversation.between(sender.id, recipient.id).empty?
+      Conversation.between(sender.id, recipient.id).first
+    else
+      Conversation.create(sender: sender, recipient: recipient)
+    end
+    render json: { id: @conversation.id }
   end
 end
