ac31d75e139ead59f4cf353675d89bc7b7ce1739
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-02-21 01:10:26 +0600

    add csrf protection

--- app/assets/javascripts/conversations.coffee
+++ app/assets/javascripts/conversations.coffee
@@ -1,5 +1,11 @@
 window.client = new Faye.Client('/faye')
 
+client.addExtension
+  outgoing: (message, callback) ->
+    message.ext = message.ext || {}
+    message.ext.csrfToken = $('meta[name=csrf-token]').attr('content')
+    callback(message)
+
 Chat = {}
 
 class Chat.UI
---
+++ app/middleware/csrf_protection.rb
@@ -0,0 +1,31 @@
+class CsrfProtection
+  def incoming(message, request, callback)
+    session_token = request.session['_csrf_token']
+    message_token = message['ext'] && message['ext'].delete('csrfToken')
+
+    unless valid_token?(session_token, message_token)
+      message['error'] = '401::Access denied'
+    end
+
+    callback.call(message)
+  end
+
+  private
+
+  def valid_token?(session_token, message_token)
+    masked_token = Base64.strict_decode64(message_token)
+    real_token = Base64.strict_decode64(session_token)
+
+    one_time_pad = masked_token[0...32]
+    encrypted_csrf_token = masked_token[32..-1]
+    csrf_token = xor_byte_string(one_time_pad, encrypted_csrf_token)
+
+    ActiveSupport::SecurityUtils.secure_compare(csrf_token, real_token)
+  rescue
+    false
+  end
+
+  def xor_byte_string(s1, s2)
+    s1.bytes.zip(s2.bytes).map { |(c1,c2)| c1 ^ c2 }.pack('c*')
+  end
+end
