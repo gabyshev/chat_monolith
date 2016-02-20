df57e889d11c99b6b18e104dc97c48aac68ff39c
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-02-20 23:39:18 +0600

    routes: messages resources nested inside conversations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :conversations, only: [:index, :create] do
+    resources :messages, only: [:index, :show]
+  end
   root 'conversations#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
