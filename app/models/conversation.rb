63ef62782bb3d31d977a369c0e09747c765902da
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-03-24 23:15:52 +0600

    add validations to user and conversation models

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,12 +3,21 @@ class Conversation < ActiveRecord::Base
   belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
   has_many :messages, dependent: :destroy
 
-  scope :between, -> (sender_id, recipient_id ) do
+  validates_presence_of :sender, :recipient
+  validate :recipient_and_sender
+
+  scope :between, -> (sender_id, recipient_id) do
     where(
       "(conversations.sender_id = ? AND conversations.recipient_id =?)
         OR
       (conversations.sender_id = ? AND conversations.recipient_id =?)",
-      sender_id,recipient_id, recipient_id, sender_id
+      sender_id, recipient_id, recipient_id, sender_id
     )
+  end
+
+  private
+
+  def recipient_and_sender
+    errors.add(:base, 'recipient and sender can\'t be same') if sender == recipient
   end
 end
