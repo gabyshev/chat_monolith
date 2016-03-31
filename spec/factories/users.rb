984b4ebe624dc8a2ce5c06baba9b10e55cfeb64a
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-03-31 17:07:16 +0600

    update factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :user do
+  factory :user, aliases: [:sender, :recipient] do
     first_name 'John'
     last_name 'Black'
     sequence(:email) { |n| "email_#{n}@test.com" }
