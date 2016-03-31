d34ca726ec4d9435aec2dbad6e492fda6d85280f
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-03-31 17:08:13 +0600

    update conversation model spec

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Conversation, type: :model do
-  let(:sender) { create :user }
-  let(:recipient) { create :user }
+  let(:sender)        { create :user }
+  let(:recipient)     { create :user }
+  let!(:conversation) { create :conversation, sender: sender, recipient: recipient }
 
   describe '#validations' do
     it { should validate_presence_of(:recipient) }
@@ -12,7 +13,7 @@ RSpec.describe Conversation, type: :model do
   describe '#associations' do
     it { should have_many(:messages) }
 
-    it 'sender and recipient can\'t be same' do
+    it 'sender and recipient can\'t be the same' do
       conversation = Conversation.new(sender: sender, recipient: sender)
       expect(conversation).to_not be_valid
     end
@@ -23,7 +24,11 @@ RSpec.describe Conversation, type: :model do
     end
 
     context '.between' do
-
+      it 'should be the same conversation' do
+        sender_recipient = Conversation.between(sender.id, recipient.id)
+        recipient_sender = Conversation.between(recipient.id, sender.id)
+        expect(sender_recipient.first.id).to eq(recipient_sender.first.id)
+      end
     end
   end
 end
